095548aef26a6cdd490dfe429414330ab8ef30be
code-beginner-98 <92854522+code-beginner-98@users.noreply.github.com>
2025-05-27 16:13:44 +0200

    fixed logic, errors, comments, added print, simple initial state

--- src/game_state.rs
+++ src/game_state.rs
@@ -1,62 +1,71 @@
-struct GameState {
+pub struct GameState {
     generation: u8,
-    field: [u8; 10]
+    pub field: [usize; 10]
 }
 
 impl GameState {
-    fn init() -> GameState{
-        /// initialize a limited playing field (currently 2D)
-        /// of 10 spaces.
+    /// initialize a limited playing field (currently 2D)
+    /// of 10 spaces.
+    pub fn init() -> GameState{
         return GameState {
             generation: 0,
             field: [1, 0, 1, 0, 1, 0, 1, 0, 1, 0]
         }
     }
 
-    fn add_space(){
-        /// increase rendering space, later
+    /// TO DO: increase rendering space
+    pub fn add_space(){
     }
 
-    fn update(&self) -> GameState {
-        /// create new GameState, where all positions are
-        /// decided by their respective values from the
-        /// previous generation. also increase generation
-        /// value.
+    /// Calculate future value of each cell and assign
+    /// new value to field of GameState.
+    /// Rules:
+    /// 2 Neighbours -> toggle state
+    /// 1 Neighbours -> stay the same
+    /// 0 Neighbours -> become 0
+    pub fn update(&mut self) {
         
-        // calculate field value (amount of neighbours)
-        // for all fields.
-        let field_values = [0, self.field.len()];
-        let field_new = [0, 10];
-
-        // read out field and calc field_values
-        for i in self.field {
-            field_values[i] = field[i-1] + field[i+1]
+        // calculate amount of neighbours
+        // for all cells
+        let mut cell_neighbours: [u8; 10] = [0; 10];
+        let mut field_new = [0; 10];
+        let mut n = 0;
+        for _ in self.field {
+            cell_neighbours[n] = if n == 0 {
+                (self.field[self.field.len()-1] + self.field[n+1]) as u8
+            } else if n == self.field.len()-1 {
+                (self.field[n-1] + self.field[self.field.len()-1]) as u8
+            } else {
+                (self.field[n-1] + self.field[n+1]) as u8
+            };
+            n += 1;
         }
-
-        // fill new field by rules:
-        // Toggle 1/0 if value = 2
-        // Keep if value = 1
-        // 1->0 if value = 0
-        for i in range(0, field_values.len()) {
-            if i = 2 {
-                if self.field[i] = 0 {
-                    field_new[i] = 1;
+        // print!("nbrs: {:?}", cell_neighbours);
+        // fill new field
+        n = 0;
+        for i in cell_neighbours {
+            if i == 2 {
+                if self.field[n] == 0 {
+                    field_new[n] = 1;
                 }
                 else {
-                    field_new[i] = 0;
+                    field_new[n] = 0;
                 }
-            }
-            if i = 1 {
-                field_new[i] = self.field[1];
+            } else if i == 1 {
+                field_new[n] = self.field[n];
             }
             else {
-                field_new[i] = 0;
+                field_new[n] = 0;
             }
+            n += 1;
         }
+        // print!("new: {:?}", field_new);
 
-        return GameState{
-            generation: self.generation + 1,
-            field: field_new
-        };
+        self.generation += 1;
+        self.field = field_new;
+    }
+
+    pub fn print(&self) {
+        println!("state: {:?}", self.field)
     }
 }
