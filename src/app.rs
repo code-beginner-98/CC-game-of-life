a9a503ad34f525932617bd4c3716c4f6230c8ec5
code-beginner-98 <friedrich-rosenkranz@gmx.net>
2025-06-11 22:03:26 +0200

    Added multithreading to decouple UI and Game updates

--- src/app.rs
+++ src/app.rs
@@ -1,15 +1,15 @@
 use std::{
-    thread::sleep,
-    time::{Duration, Instant},
+    sync::{Arc, Mutex},
 };
-use egui::{Button, Color32, Pos2, Rect, Sense, Vec2};
+use egui::{Button, Color32, Pos2, Rect, Sense, Slider, Vec2};
 use crate::game_state::GameState;
 
 pub struct App {
-    pub state: GameState,
-    pub paused: bool,
+    pub shared_state: Arc<Mutex<GameState>>,
+    pub shared_paused: Arc<Mutex<bool>>,
     pub pan_offset: Vec2,
     pub zoom: f32,
+    pub shared_speed: Arc<Mutex<u8>>,
 }
 
 // Going with egui/eframe might be the worst mistake I will make in a long time due
@@ -18,21 +18,30 @@ pub struct App {
 // egui has it. It's a learning process, people, deal with it.
 impl eframe::App for App {
     fn update(&mut self, ctx: &egui::Context, _frame: &mut eframe::Frame) {
-        let start = Instant::now();
+        // println!("updating ui.");
+
+        // reads out temporary values so the state doesn't lock forever.
+        let (field_width, field_height) = {
+            let state = self.shared_state.lock().unwrap();
+            (state.field.len(), state.field[0].len())
+        };
+
         egui::CentralPanel::default().show(ctx, |ui| {
             // Here go all my UI elements
             // TODO: put labels + buttons in seperate widget
             ui.label("Welome to the Game of Life");
-            let button_state = if self.paused { "Resume"} else {"Pause"};
-            if ui.add(Button::new(button_state)).clicked() {
-                self.paused = !self.paused;
+            if let Ok(mut paused) = self.shared_paused.lock() {
+                let button_state = if *paused { "Resume"} else {"Pause"};
+                if ui.add(Button::new(button_state)).clicked() {
+                    *paused = !*paused;
+                }
+            }
+            
+            // TODO: add slider for game speed: 5 speeds, refresh rate = 0.5s, 0.25s, 0.1s, 0.04s, 0.01s
+            if let Ok(mut speed) = self.shared_speed.lock() {
+                ui.add(Slider::new(&mut *speed, 1..=5).text("Simulation Speed"));
             }
             ui.label(format!("Zoom: {}", self.zoom));
-            // Testing Area Start ---vvv
-
-            
-
-            // Testing Area End ---^^^
 
             // playing field
             let field_size = 1000f32; // TODO: make as big as window (resizing)
@@ -43,40 +52,34 @@ impl eframe::App for App {
 
             // Zoom
             self.zoom *= ctx.input(|i|i.zoom_delta());
-            if self.zoom < field_size / 50.0 / self.state.field.len() as f32 {
-                self.zoom = field_size / 50.0 / self.state.field.len() as f32;
+            if self.zoom < field_size / 50.0 / field_width as f32 {
+                self.zoom = field_size / 50.0 / field_width as f32;
             }
 
             // Panning
             self.pan_offset += response.drag_delta();
             if self.pan_offset.x > 0.0 {self.pan_offset.x = 0.0};
             if self.pan_offset.y > 0.0 {self.pan_offset.y = 0.0};
-            if self.pan_offset.x < ((self.state.field.len() as f32) * cell_size - field_size)*-1.0 {self.pan_offset.x = ((self.state.field.len() as f32) * cell_size - field_size)*-1.0}; // TODO: rewrite this crap.
-            if self.pan_offset.y < ((self.state.field[0].len() as f32) * cell_size - field_size)*-1.0 {self.pan_offset.y = ((self.state.field.len() as f32) * cell_size - field_size)*-1.0}; // TODO: rewrite this crap.
+            if self.pan_offset.x < ((field_width as f32) * cell_size - field_size)*-1.0 {self.pan_offset.x = ((field_width as f32) * cell_size - field_size)*-1.0}; // TODO: rewrite this crap.
+            if self.pan_offset.y < ((field_height as f32) * cell_size - field_size)*-1.0 {self.pan_offset.y = ((field_width as f32) * cell_size - field_size)*-1.0}; // TODO: rewrite this crap.
             // ui.label(format!("Scene rect: {:#?}", &mut self.pan_offset));
 
             let cell_vec = Vec2::new(cell_size, cell_size);
             let top = if ((self.pan_offset.y * -1f32 / cell_size) as i32) < 0 {0} else {(self.pan_offset.y * -1f32 / cell_size) as usize}; // TODO: Rewrite as closure maybe?
             let left = if ((self.pan_offset.x * -1f32 / cell_size) as i32) < 0 {0} else {(self.pan_offset.x * -1f32 / cell_size) as usize}; // TODO: Rewrite as closure maybe?
             // TODO: add zooming - make range depend on zoomfactor
-            for (ind_y, y) in (top..top+((field_size/cell_size) as usize)).into_iter().enumerate() {
-                for (ind_x, x) in (left..left+((field_size/cell_size) as usize)).into_iter().enumerate() {
-                    let cell_pos = Pos2::new(ind_x as f32 * cell_size, ind_y as f32 * cell_size);
-                    if self.state.field[x][y] == 1 {
-                        painter.rect_filled(Rect::from_min_size(cell_pos, cell_vec), 0.0, Color32::BLACK);
+            if let Ok(state) = self.shared_state.lock() {
+                for (ind_y, y) in (top..top+((field_size/cell_size) as usize)).into_iter().enumerate() {
+                    for (ind_x, x) in (left..left+((field_size/cell_size) as usize)).into_iter().enumerate() {
+                        let cell_pos = Pos2::new(ind_x as f32 * cell_size, ind_y as f32 * cell_size);
+                        if state.field[x][y] == 1 {
+                            painter.rect_filled(Rect::from_min_size(cell_pos, cell_vec), 0.0, Color32::BLACK);
+                        }
                     }
                 }
             }
         });
-        // Game Logic hook in, loop every some ms
-        let refresh_rate = Duration::from_millis(20);
-        if refresh_rate > start.elapsed() {
-            let pause = refresh_rate - start.elapsed();
-            sleep(pause);
-        }
-        if !self.paused {
-            self.state.update()
-        }
+
         ctx.request_repaint();
     }
 }
